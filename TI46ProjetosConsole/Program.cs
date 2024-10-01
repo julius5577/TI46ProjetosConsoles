f1c2e9b357bb39e0fbf8a19e53fd0fdc164d9f74
JULIO CESAR GOMES <julio.cgomes8@senacsp.edu.br>
2024-09-30 22:20:55 -0300

    primeiro exercicio , falta a soma

--- TI46ProjetosConsole/Program.cs
+++ TI46ProjetosConsole/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,8 +11,24 @@ namespace TI46ProjetosConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ola mundo !!!!");
-            Console.WriteLine(" Eu sou o Júlio ");
+            string Nome;
+            decimal nota1,nota2,nota3,nota4;
+
+            Console.WriteLine("digite seu nome: ");
+
+            Nome = Console.ReadLine();
+
+            Console.WriteLine(" Seja bem vindo "+Nome);
+
+            Console.WriteLine(" digite as notas do aluno "+Nome);
+
+            nota1 = decimal.Parse(Console.ReadLine());
+            nota2 = decimal.Parse(Console.ReadLine());
+            nota3 = decimal.Parse(Console.ReadLine());
+            nota4 = decimal.Parse(Console.ReadLine());
+
+
+
 
 
 
